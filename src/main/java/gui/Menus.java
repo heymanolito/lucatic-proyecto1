536ffcdb56cc36c3048b81b37c13f00fe81ab6f1
unknown <subirats2000@gmail.com>
2021-12-28 10:38:23 +0100

    He creado los métodos imprimirMenu() y darBienvenida() en la clase Menu

--- src/main/java/gui/Menus.java
+++ src/main/java/gui/Menus.java
@@ -2,16 +2,16 @@ package gui;
 
 public class Menus {
 	
-	public Menus() {
+	public static void imprimirMenu() {
 		
-	}
-	
-	public void imprimirMenu() {
 		System.out.println(" //¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\ ");
-		System.out.println(" |			   *Escoge una opcion: ");
-		System.out.println(" |			 1. Dar de alta un juego.");
-		System.out.println(" |			 2. Ver listado de juegos.");
-		System.out.println(" |			 3. Ver listas específicas");
+		System.out.println(" |			   *Escoge una opcion:   			  |");  
+		System.out.println(" |			 1. Dar de alta un juego.  			  |");
+		System.out.println(" |			 2. Ver listado de juegos.  	      |");
+		System.out.println(" |			 3. Ver listas específicas.  		  |");
 		System.out.println(" \\______________________________________________// ");
+	}
+	public static void darBienvenida() {
+		System.out.println(" Bienvenido al gestor de juegos de LucaSteam "  );
 	}
 }
