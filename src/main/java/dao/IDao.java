4cb7210e13b0c30fe0a54182a73fcf91d6982219
Josmax <josemanuelmolinallam@gmail.com>
2021-12-28 10:52:55 +0100

    He creado la interfaz IDao

--- src/main/java/dao/IDao.java
+++ src/main/java/dao/IDao.java
@@ -1,4 +1,55 @@
 package dao;
 
+import modelo.Juego;
+
+/**
+ * 
+ * @author Grupo01: Jose Manuel Molina
+ *
+ */
 public interface IDao {
+	
+	/**
+	 * 
+	 * @param juego juego que voy a dar de alta
+	 */
+	public void alta(Juego juego);
+	
+	/**
+	 * 
+	 * @param juego juego que voy a dar de baja
+	 */
+	public void baja(Juego juego);
+	
+	/**
+	 * 
+	 * @param id del juego a modificar
+	 */
+	public void modificar(Integer id);
+	
+	/**
+	 * facilita la lectura del archivo CVS
+	 */
+	public void leer();
+	
+	/**
+	 * Mostramos la informacion 
+	 */
+	public void listarTodo();
+	
+	/**
+	 * Borramos toda la informacion
+	 */
+	public void borrarTodo();
+	
+	/**
+	 * Serializamos la informacion del archivo
+	 */
+	public void serializar();
+	
+	/**
+	 * Deserializamos la informacion del archivo
+	 */
+	public void deserializar();
+	
 }
