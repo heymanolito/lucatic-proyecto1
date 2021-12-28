97876a9e988afc7349eea938de6615a6fa8cfd99
unknown <subirats2000@gmail.com>
2021-12-28 11:44:54 +0100

    He puesto los métodos darBienvenida() y darDespedida() en la clase Menus y he hecho un trozo de la clase LucaSteamController

--- src/main/java/controlador/LucaSteamController.java
+++ src/main/java/controlador/LucaSteamController.java
@@ -1,6 +1,31 @@
 package controlador;
 
+import gui.Menus;
+
+/**
+ * 
+ * @author Grupo01 Jaume
+ *
+ */
 
 public class LucaSteamController {
 	
+	/**
+	 * Da la bienvenida e imprime el menú
+	 */
+	public static void start() {
+		Menus.darBienvenida();
+		Menus.imprimirMenu();
+	}
+	
+	public static void elegirOpcion() {
+		
+	}
+	
+	/**
+	 * Cierra el menú
+	 */
+	public static void stop() {
+		Menus.darDespedida();
+	}
 }
--- src/main/java/gui/Menus.java
+++ src/main/java/gui/Menus.java
@@ -1,17 +1,38 @@
 package gui;
 
+/**
+ * @author Grupo01 Jaume
+ */
 public class Menus {
+
+	/**
+	 * Este método imprime el menú y todas sus opciones
+	 */
 	
 	public static void imprimirMenu() {
 		
-		System.out.println(" //¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\ ");
+		
+		System.out.println(" //¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\");
 		System.out.println(" |			   *Escoge una opcion:   			  |");  
 		System.out.println(" |			 1. Dar de alta un juego.  			  |");
 		System.out.println(" |			 2. Ver listado de juegos.  	      |");
 		System.out.println(" |			 3. Ver listas específicas.  		  |");
-		System.out.println(" \\______________________________________________// ");
+		System.out.println(" |           0. Salir                             |");
+		System.out.println(" \\______________________________________________//");
 	}
+	
+	/**
+	 * Este método imprime el mensaje de bienvenida
+	 */
 	public static void darBienvenida() {
 		System.out.println(" Bienvenido al gestor de juegos de LucaSteam "  );
+	}
+	
+	/**
+	 * Este método imprime el mensaje de despedida
+	 */
+	
+	public static void darDespedida() {
+		System.out.println(" Hasta la próxima ");
 	}
 }
