a505c29d07d87294ca25a1f036a84fcecb3ab279
alumno <subirats2000@gmail.com>
2021-12-28 10:34:36 +0100

    He creado los métodos imprimirMenu() y darBienvenida() en la clase Menu

--- src/main/java/gui/Menus.java
+++ src/main/java/gui/Menus.java
@@ -1,4 +1,17 @@
 package gui;
 
 public class Menus {
+	
+	public Menus() {
+		
+	}
+	
+	public void imprimirMenu() {
+		System.out.println(" //¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\ ");
+		System.out.println(" |			   *Escoge una opcion: ");
+		System.out.println(" |			 1. Dar de alta un juego.");
+		System.out.println(" |			 2. Ver listado de juegos.");
+		System.out.println(" |			 3. Ver listas específicas");
+		System.out.println(" \\______________________________________________// ");
+	}
 }
