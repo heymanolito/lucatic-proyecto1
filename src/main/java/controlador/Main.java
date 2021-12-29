5fd1cbe9dfc171c805f4d34b74dd955e39f5ca76
unknown <n.borregolopez@gmail.com>
2021-12-29 21:18:19 +0100

    un poquito de main

--- src/main/java/controlador/Main.java
+++ src/main/java/controlador/Main.java
@@ -1,6 +1,20 @@
 package controlador;
 
+/**
+ * Nombre de la clase: Main. Descripcion: En esta clase se ejecuta el programa.
+ * Fecha: 29/12/21.
+ * 
+ * @version LucaSteam1.0.0
+ * @author Grupo1 Natalia Borrego Lopez
+ */
+
 public class Main {
+
+	/**
+	 * Método principal para ejecutar el programa.
+	 * 
+	 * @param args Argumentos de la línea de comando
+	 **/
 
 	public static void main(String[] args) {
 		LucaSteamController programa = new LucaSteamController();
